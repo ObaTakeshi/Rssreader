9d392ad999bf76962b21e38a3757f677c9329aed
oba <oba@Tokumaru-no-MacBook-Pro.local>
2017-06-14 13:03:36 +0900

    旧URLの削除

--- RssReader/Feed.swift
+++ RssReader/Feed.swift
@@ -9,11 +9,6 @@
 import Foundation
 import RealmSwift
 
-enum Setting {
-    static let RssUrl = "http://feeds.feedburner.com/hatena/b/hotentry"
-    static let RssUrl1 = "http://www3.nhk.or.jp/rss/news/cat0.xml"
-}
-
 class Feed: Object {
     dynamic var name = ""
     dynamic var url = ""
